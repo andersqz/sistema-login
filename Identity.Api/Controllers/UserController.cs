96c29191dde02e37737d78ea97c593175c3d68d9
andersqz <anderson.carvalho@ufn.edu.br>
2026-09-06 23:37:47 -0300

    retira try/catch de UserController

--- Identity.Api/Controllers/UserController.cs
+++ Identity.Api/Controllers/UserController.cs
@@ -33,80 +33,40 @@ namespace Identity.Api.Controllers
         public async Task<IActionResult> GetById(int id)
         {
             _logger.LogInformation("Buscando o User ID {id}", id);
-            try
-            {
-                UserResponseDto? user = await _service.SelectById(id);
-                return Ok(user);
-            }
-            catch (NotFoundException e)
-            {
-                return NotFound(e.Message);
-            }
+            UserResponseDto? user = await _service.SelectById(id);
+            return Ok(user);
         }
 
         [HttpGet("{email}")]
         public async Task<IActionResult> GetByEmail(string email)
         {
             _logger.LogInformation("Buscando o User E-mail {email}", email);
-            try
-            {
-                UserResponseDto? user = await _service.SelectByEmail(email);
-                return Ok(user);
-            }
-            catch (NotFoundException e)
-            {
-                return NotFound(e.Message);
-            }
+            UserResponseDto? user = await _service.SelectByEmail(email);
+            return Ok(user);
         }
 
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] UserRequestDto request)
         {
             _logger.LogInformation("Criando um novo User");
-            try
-            {
-                UserResponseDto response = await _service.Create(request);
-                return CreatedAtAction(nameof(GetById), new {id = response.Id}, response);
-            }
-            catch (RuleBusinessException e)
-            {
-                return Conflict(e.Message);
-            }
-
+            UserResponseDto response = await _service.Create(request);
+            return CreatedAtAction(nameof(GetById), new {id = response.Id}, response);
         }
 
         [HttpPut("{id:int}")]
         public async Task<IActionResult> Put([FromBody] UserRequestUpdateDto request, int id)
         {
             _logger.LogInformation("Atualizando o User ID {id}", id);
-            try
-            {
-                bool IsUpdated = await _service.Update(request, id);
-                return NoContent();
-            }
-            catch (NotFoundException e)
-            {
-                return NotFound(e.Message);
-            }
-            catch (RuleBusinessException e)
-            {
-                return Conflict(e.Message);
-            }
+            bool IsUpdated = await _service.Update(request, id);
+            return NoContent();
         }
 
         [HttpDelete]
         public async Task<IActionResult> Delete(int id)
         {
             _logger.LogInformation("Deletando o User ID {id}", id);
-            try
-            {
-                await _service.Delete(id);
-                return NoContent();
-            }
-            catch (NotFoundException e)
-            {
-                return NotFound(e.Message);
-            }
+            await _service.Delete(id);
+            return NoContent();
         }
     }
 }
